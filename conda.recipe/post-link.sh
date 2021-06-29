a0fb8a52fc8ac56f53c626d1cdae5a0897aa6e4a
fragoso <fragoso@ebi.ac.uk>
2021-06-29 10:18:20 +0100

    clean post-link.sh

--- conda.recipe/post-link.sh
+++ conda.recipe/post-link.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-env > ~/env.test.txt
 wget ftp://ftp.ebi.ac.uk/pub/software/unix/iprscan/5/5.52-86.0/interproscan-5.52-86.0-64-bit.tar.gz
 wget ftp://ftp.ebi.ac.uk/pub/software/unix/iprscan/5/5.52-86.0/interproscan-5.52-86.0-64-bit.tar.gz.md5
 md5sum -c interproscan-5.52-86.0-64-bit.tar.gz.md5
@@ -15,10 +14,3 @@ ln -s ${PKG_DIR}/interproscan-5.52-86.0/interproscan.sh ${CONDA_PREFIX}/bin
 cd ${PKG_DIR}/interproscan-5.52-86.0/
 $PY initial_setup.py 
 
-
-
-
-
-#emerald_download_data 
-#ln -s $SP_DIR/emeraldbgc/interproscan-5.52-86.0/interproscan.sh $PREFIX/bin/interproscan.sh
-#ln -s ~/interproscan.sh $PREFIX/bin/interproscan.sh

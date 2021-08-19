0e6c93cc0c1c9c19a8c81577635e37e00a17b31d
fragoso <fragoso@ebi.ac.uk>
2021-08-19 11:12:27 +0100

    add unittest

--- test/test_unit_emerald.py
+++ test/test_unit_emerald.py
@@ -1,13 +1,7 @@
 from emeraldbgc import _cli
 import pytest
 
-
 def testCliHelp():
     with pytest.raises(SystemExit) as e:
         _cli.main(['--help'])
     assert e.value.code == 0
-
-def testCliError():
-    with pytest.raises(SystemExit) as e:
-        _cli.main(['invalid'])
-    assert e.value.code == 2
